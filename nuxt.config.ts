c43634e65c91c8a713d4f064bd80859cf0cd9be9
Javier Toledo <usual.song3938@fastmail.com>
2026-04-02 05:52:56 +0000

    fix: restaurar modulos y agregar binding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nitro-cloudflare-dev"],
+  modules: [
+    "nitro-cloudflare-dev",
+    '@nuxt/content',
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+//    '@nuxtjs/tailwindcss',
+    'nuxt-studio',
+    '@nuxtjs/color-mode'
+  ],
   devtools: { enabled: true },
   compatibilityDate: "2025-05-15",
   studio: {
