24f306b4bbf624e48cae808e6de21948b0297328
Javier Toledo <usual.song3938@fastmail.com>
2026-04-02 05:26:01 +0000

    feat: despliegues con cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-//    '@nuxtjs/tailwindcss',
-    'nuxt-studio',
-    '@nuxtjs/color-mode'
-  ],
+  modules: ["nitro-cloudflare-dev"],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
   studio: {
@@ -26,6 +18,13 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       autoSubfolderIndex: false
+    },
+
+    preset: "cloudflare_module",
+
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
     }
   },
   css: ['./app/assets/css/main.css'],
